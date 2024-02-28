d417ca376ed3cee0addb73bc680f916102477b11
HP <HP@192.168.1.38>
2024-02-28 04:04:55 -0800

    dev commit

--- cS/src/main/java/com/tcs/service/App.java
+++ cS/src/main/java/com/tcs/service/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        App obj=new App();
+        obj.hospitalBill(41, 74);
+        System.out.println(obj.hospitalBill(41, 74));
+    }
+    public int hospitalBill(int x, int y)
+    {
+    	return x+y;
     }
 }
