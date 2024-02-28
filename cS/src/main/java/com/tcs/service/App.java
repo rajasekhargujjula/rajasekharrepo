50690825acb2f8c2b8d0b9812be89decea10d314
HP <HP@192.168.1.38>
2024-02-28 04:17:55 -0800

    hi

--- cS/src/main/java/com/tcs/service/App.java
+++ cS/src/main/java/com/tcs/service/App.java
@@ -6,16 +6,26 @@ package com.tcs.service;
  */
 public class App 
 {
-    public static void main( String[] args )
+	
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         
         App obj=new App();
         obj.hospitalBill(41, 74);
+        obj.hospitalDetails();
         System.out.println(obj.hospitalBill(41, 74));
     }
     public int hospitalBill(int x, int y)
     {
     	return x+y;
+    }
+    
+    public String hospitalDetails()
+    {
+    
+       return "hospitalDetails";
+    		   
+    	
     }
 }
